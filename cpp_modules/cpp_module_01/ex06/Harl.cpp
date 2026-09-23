de2f8fe265d537e7d1a499db9c2e465feb98c481
Dyrina <dylankong.dev@proton.me>
2026-09-23 16:21:31 +0800

    misc(cpp01): add 42header to ex06

--- cpp_modules/cpp_module_01/ex06/Harl.cpp
+++ cpp_modules/cpp_module_01/ex06/Harl.cpp
@@ -1,38 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydylan-k <ydylan-k@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 20:15:55 by ydylan-k          #+#    #+#             */
+/*   Updated: 2026/09/23 16:17:52 by ydylan-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 #include <iostream>
 
-void Harl::debug(void) {
-    std::cout << "[ DEBUG ]\nI love having extra bacon for my burger!\n";
-}
-
-void Harl::info(void) {
-    std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money.\n";
-}
-
-void Harl::warning(void) {
-    std::cout << "[ WARNING ]\nI think I deserve some extra bacon for free.\n";
-}
-
-void Harl::error(void) {
-    std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager.\n";
-}
-
 Harl::Harl() {}
+
 Harl::~Harl() {}
+
+void	Harl::debug(void)
+{
+    std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
+
+void	Harl::info(void)
+{
+    std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void	Harl::warning(void)
+{
+    std::cout << "[ WARNING ]\nI think I deserve some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error(void)
+{
+    std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
+}
 
 void Harl::complain(std::string level)
 {
-    std::string levels[] = {
-        "DEBUG",
-        "INFO",
-        "WARNING",
-        "ERROR"
+    std::string levels[] =
+    {
+    	"DEBUG",
+     	"INFO",
+      	"WARNING",
+       	"ERROR"
     };
 
     int i = 0;
-	while (i < 4 && levels[i] != level) {
+	while (i < 4 && levels[i] != level) 
 		++i;
-	}
 	switch(i)
 	{
 		case 0:
@@ -51,5 +68,4 @@ void Harl::complain(std::string level)
 			std::cout << "[ Probably complaining about insignificant problems ]\n";
 			break ;
 	}
-
 }
--- cpp_modules/cpp_module_01/ex06/Harl.hpp
+++ cpp_modules/cpp_module_01/ex06/Harl.hpp
@@ -1,20 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydylan-k <ydylan-k@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 20:15:57 by ydylan-k          #+#    #+#             */
+/*   Updated: 2026/09/23 16:20:20 by ydylan-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HARL_HPP
 # define HARL_HPP
 
-#include <string>
+# include <string>
 
 class	Harl
 {
-private:
-    void	debug(void);
-    void	info(void);
-    void	warning(void);
-    void	error(void);
-
-public:
-    Harl();
-    ~Harl();
-    void	complain(std::string level);
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
 };
 
 #endif
