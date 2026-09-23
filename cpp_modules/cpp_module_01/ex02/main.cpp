00bf31e67eec9ef4f49619e19c6e5ef6174a37f3
Dyrina <dylankong.dev@proton.me>
2026-09-23 15:23:40 +0800

    misc(cpp01): add 42header to ex02

--- cpp_modules/cpp_module_01/ex02/main.cpp
+++ cpp_modules/cpp_module_01/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydylan-k <ydylan-k@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/14 16:23:06 by ydylan-k          #+#    #+#             */
+/*   Updated: 2026/09/23 15:23:03 by ydylan-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 
@@ -7,10 +19,12 @@ int	main(void)
 	std::string*	stringPTR = &brain;
 	std::string&	stringREF = brain;
 
+	std::cout << "Memory address:\n";
 	std::cout << "stringADR: " << &brain << "\n";
 	std::cout << "stringPTR: " << &stringPTR << "\n";
-	std::cout << "stringREF: " << &stringREF << "\n";
+	std::cout << "stringREF: " << &stringREF << "\n\n";
 
+	std::cout << "Value:\n";
 	std::cout << "stringADR: " << brain << "\n";
 	std::cout << "stringPTR: " << *stringPTR << "\n";
 	std::cout << "stringREF: " << stringREF << "\n";
