dd51a8171dc4bfe2d7301ab5d5d31d4aeab0d750
Dyrina <dylankong.dev@proton.me>
2026-09-23 16:17:39 +0800

    misc(cpp01): add 42header to ex05

--- cpp_modules/cpp_module_01/ex05/Harl.cpp
+++ cpp_modules/cpp_module_01/ex05/Harl.cpp
@@ -1,51 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydylan-k <ydylan-k@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 19:38:41 by ydylan-k          #+#    #+#             */
+/*   Updated: 2026/09/23 16:05:03 by ydylan-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 #include <iostream>
 
-void Harl::debug(void) {
-    std::cout << "[ DEBUG ]\nI love having extra bacon for my burger!\n";
-}
-
-void Harl::info(void) {
-    std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money.\n";
-}
-
-void Harl::warning(void) {
-    std::cout << "[ WARNING ]\nI think I deserve some extra bacon for free.\n";
-}
-
-void Harl::error(void) {
-    std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager.\n";
-}
-
 Harl::Harl() {}
+
 Harl::~Harl() {}
 
-void Harl::complain(std::string level) {
-    // 1. Define a typedef for the member function pointer to keep syntax readable
-    typedef void (Harl::*HarlMemFn)(void);
+void	Harl::debug(void)
+{
+    std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
 
-    // 2. Map levels to member function pointers
-    std::string levels[] = {
-        "DEBUG",
-        "INFO",
-        "WARNING",
-        "ERROR"
-    };
+void	Harl::info(void)
+{
+    std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
 
-    HarlMemFn functions[] = {
-        &Harl::debug,
-        &Harl::info,
-        &Harl::warning,
-        &Harl::error
-    };
+void	Harl::warning(void)
+{
+    std::cout << "[ WARNING ]\nI think I deserve some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n";
+}
 
-    // 3. Search and dispatch
-    for (int i = 0; i < 4; ++i) {
-        if (levels[i] == level) {
-            (this->*functions[i])();
-            return;
-        }
-    }
+void	Harl::error(void)
+{
+    std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
+}
 
+void	Harl::complain(std::string level)
+{
+	typedef void	(Harl::*HarlMemFn)(void);
+
+	std::string	levels[] =
+	{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	HarlMemFn	functions[] =
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	for (int i = 0; i < 4; ++i)
+	{
+		if (levels[i] == level)
+		{
+			(this->*functions[i])();
+			return;
+		}
+	}
     std::cout << "[ Probably complaining about insignificant problems ]\n";
 }
--- cpp_modules/cpp_module_01/ex05/Harl.hpp
+++ cpp_modules/cpp_module_01/ex05/Harl.hpp
@@ -1,20 +1,31 @@
-#ifndef HARL_HPP
-#define HARL_HPP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydylan-k <ydylan-k@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 19:38:37 by ydylan-k          #+#    #+#             */
+/*   Updated: 2026/09/23 16:05:52 by ydylan-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <string>
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
 
 class	Harl
 {
-private:
-    void	debug(void);
-    void	info(void);
-    void	warning(void);
-    void	error(void);
-
-public:
-    Harl();
-    ~Harl();
-    void	complain(std::string level);
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
 };
 
 #endif
